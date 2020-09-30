5116eb5d313517d2272c48b2e8a21f0141c89de0
VSRUJANA <srujana.valavala@gmail.com>
2020-09-30 08:09:56 +0530

    [srujana] Add. Program to calculate employee daily wage for full-time and part-time employee added

--- Employee_Wage_Computation_OOPS/Program.cs
+++ Employee_Wage_Computation_OOPS/Program.cs
@@ -7,20 +7,29 @@ namespace Employee_Wage_Computation_OOPS
         static void Main(string[] args)
         {
             //constants
-            int IS_PRESENT = 1;
+            int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 2;
             int WAGE_PER_HOUR = 20;
-            //variables
+            //Variables
             int empHrs = 0;
             int empWage = 0;
-            //Computation of wage
             Random rand = new Random();
-            int empCheck = rand.Next(0, 2);
-            if (empCheck == IS_PRESENT)
+            int empCheck = rand.Next(0, 3);
+            //Computation of wage
+            if (empCheck == IS_FULL_TIME)
             {
                 empHrs = 8;
             }
+            else if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;  //Assuming part time hours as 4
+            }
+            else
+            {
+                empHrs = 0;
+            }
             empWage = empHrs * WAGE_PER_HOUR;
-            Console.WriteLine("Employee wage :" + empWage);
+            Console.WriteLine("Employee Wage: " + empWage);
         }
     }
 }
