07189b8a5cb3d3c44fbfb1476844eda10705bc2a
VSRUJANA <srujana.valavala@gmail.com>
2020-09-30 07:57:54 +0530

    Add project files.

---
+++ Employee_Wage_Computation_OOPS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Employee_Wage_Computation_OOPS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
