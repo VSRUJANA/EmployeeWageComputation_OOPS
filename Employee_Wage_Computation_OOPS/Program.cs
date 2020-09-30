91b465386e2a17e624b546ef5ef1c5edd21ace7b
VSRUJANA <srujana.valavala@gmail.com>
2020-09-30 08:01:34 +0530

    [srujana] Add. Program to display welcome message added

--- Employee_Wage_Computation_OOPS/Program.cs
+++ Employee_Wage_Computation_OOPS/Program.cs
@@ -6,7 +6,7 @@ namespace Employee_Wage_Computation_OOPS
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Employee Wage Computation Program !");
         }
     }
 }
