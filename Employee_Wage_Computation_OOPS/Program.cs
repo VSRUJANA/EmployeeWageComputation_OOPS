9c40726bf26e19d94a0ec1721bc9c9f0d471b6ae
VSRUJANA <srujana.valavala@gmail.com>
2020-09-30 08:13:19 +0530

    [srujana] Add. Program to calculate employee using switch case added

--- Employee_Wage_Computation_OOPS/Program.cs
+++ Employee_Wage_Computation_OOPS/Program.cs
@@ -7,29 +7,29 @@ namespace Employee_Wage_Computation_OOPS
         static void Main(string[] args)
         {
             //constants
-            int IS_FULL_TIME = 1;
-            int IS_PART_TIME = 2;
-            int WAGE_PER_HOUR = 20;
-            //Variables
+            const int WAGE_PER_HOUR = 20;
+            const int IS_FULL_TIME = 1;
+            const int IS_PART_TIME = 2;
+            //variables
             int empHrs = 0;
             int empWage = 0;
-            Random rand = new Random();
-            int empCheck = rand.Next(0, 3);
-            //Computation of wage
-            if (empCheck == IS_FULL_TIME)
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
+            //switch case to calculate part time wages
+            switch (empCheck)
             {
-                empHrs = 8;
-            }
-            else if (empCheck == IS_PART_TIME)
-            {
-                empHrs = 4;  //Assuming part time hours as 4
-            }
-            else
-            {
-                empHrs = 0;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
             }
             empWage = empHrs * WAGE_PER_HOUR;
-            Console.WriteLine("Employee Wage: " + empWage);
+            Console.WriteLine("Employee wage :" + empWage);
         }
     }
 }
